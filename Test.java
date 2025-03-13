9439e4ae99f43fd1432160bc6b17ffb9f8f81742
Finn <fhertsch235@gmail.com>
2025-03-13 19:10:14 +0100

    initial commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String filePath = "data/marketing.csv";
+        //public List<Set<Item>> loadTransactions(String filePath) {}
+        File csvFile = new File(filePath);
+    }
+}
